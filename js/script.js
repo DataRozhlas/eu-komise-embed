01643eb3374cc5cebb62c9c3708a62fcc73517e1
timichal <michal@zlatkovsky.cz>
2019-03-15 14:24:33 +0100

    https

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import css from "./tablo.css";
 /** @jsx h */
 import { getAge } from "./datehelper";
 
-const rootUrl = "http://data.irozhlas.cz/sk-prezident-kandidati";
+const rootUrl = "https://data.irozhlas.cz/sk-prezident-kandidati";
 
 const Entry = ({
   candidate, id, visible, handleClick,
